2409b83022d5b16f53a0ec4de82ce09e712f2c4d
Nihal T <nihalt.95@gmail.com>
2023-04-27 07:15:16 +0000

    1

--- opencv_app_file/aesd_opencv_file.cpp
+++ opencv_app_file/aesd_opencv_file.cpp
@@ -82,7 +82,7 @@ int main(int argc, char** argv)
             break;
         }
     
-        imshow("Input", frame);
+        //imshow("Input", frame);
 
         #if DEBUG_SHOW
         // Display the frame in the window
